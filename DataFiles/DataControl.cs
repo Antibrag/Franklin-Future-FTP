6dab12ddef59095f0f0bda35bc45cfca085091d0
Anton <Anton@DESKTOP-H8DNGG6>
2023-06-30 12:51:09 +0300

    Добавьте файлы проекта.

---
+++ DataFiles/DataControl.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+public static class Data
+{
+    
+}
+
+
+public class DataControl
+{
+    private const string DEB_PATH = "user://DataFiles/SavesData.json";
+    public void SaveData()
+    {
+
+    }
+
+    public void LoadData() 
+    {
+
+    }
+}
