fc457ed08dc0ce5914737396e096481f295a6d13
Антон Брагин <AntonSayfer@gmail.com>
2023-07-01 13:04:15 +0300

    Add class - CurrentLevel

--- DataFiles/DataControl.cs
+++ DataFiles/DataControl.cs
@@ -6,13 +6,35 @@ public static class Data
     
 }
 
+public class CurrentLevel
+{
+    private static string _LevelName = null;
+    private static sbyte _LevelId = -128;
+
+    public static void SetLevelName(string newLevelName) { _LevelName = newLevelName; }
+    public static void SetLevelId(sbyte newLevelId) { _LevelId = newLevelId; }
+
+    public static string GetLevelName()
+    {
+        if (String.IsNullOrEmpty(_LevelName)) 
+            GD.Print("Warning! Level name is not found! Please give a name for your level!");       
+        return _LevelName;
+    }
+    public static sbyte GetLevelId()
+    {
+        if (_LevelId == -128)
+            GD.Print("Warning! Level id is not found! Please give id for your level!");
+        return _LevelId;
+    }
+}
 
 public class DataControl
 {
     private const string DEB_PATH = "user://DataFiles/SavesData.json";
+
     public void SaveData()
     {
-
+        
     }
 
     public void LoadData() 
---
+++ Scripts/LevelControl.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class LevelControl : Node
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		GD.Print(CurrentLevel.GetLevelName());
+		CurrentLevel.SetLevelName(Convert.ToString(GetMeta("LevelName")));
+        GD.Print(CurrentLevel.GetLevelName());
+    }
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
