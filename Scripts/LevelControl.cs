f9bcf5672aae8adb1da70a3fba45f4a4489b0684
Антон Брагин <AntonSayfer@gmail.com>
2023-07-02 19:00:33 +0300

    Delete debug print

--- Scripts/LevelControl.cs
+++ Scripts/LevelControl.cs
@@ -3,19 +3,10 @@ using System;
 
 public partial class LevelControl : Node
 {
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
         CurrentLevel.SetLevelName((string)GetMeta("LevelName"));
         CurrentLevel.SetLevelId((int)GetMeta("LevelId"));
 		CurrentLevel.SetLevelGridSize((Vector3[])GetMeta("GridSize"));
-		GD.Print(CurrentLevel.GetLevelGridSize());
-        GD.Print(CurrentLevel.GetLevelName());
-        GD.Print(CurrentLevel.GetLevelId());
     }
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	//public override void _Process(double delta)
-	//{ 
-	//}
 }
