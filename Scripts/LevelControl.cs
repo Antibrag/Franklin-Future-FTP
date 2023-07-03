a312aaf70f5aa854e6f73d159b8bc434bc4ae46b
Bragin Anton <antonsayfermay@gmail.com>
2023-07-03 17:58:01 +0300

    Add output level properties

--- Scripts/LevelControl.cs
+++ Scripts/LevelControl.cs
@@ -8,5 +8,17 @@ public partial class LevelControl : Node
         CurrentLevel.SetName((string)GetMeta("LevelName"));
         CurrentLevel.SetId((int)GetMeta("LevelId"));
 		CurrentLevel.SetGridSize((Vector3[])GetMeta("GridSize"));
+        CurrentLevel.SetSteps((int)GetMeta("Steps"));
+        GetCurrentLevelInfo(); 
+    }
+
+    private static void GetCurrentLevelInfo() {
+        GD.Print("/--- Current level information ---\\\n");
+        GD.Print("Current level name: " + CurrentLevel.GetName());
+        GD.Print("Current level id: " + CurrentLevel.GetId());
+        GD.Print("Current level count steps: " + CurrentLevel.GetSteps());
+        GD.Print("Current level grid size: ");
+        foreach (var step in CurrentLevel.GetGridSize()) 
+            GD.Print("\t" + step);
     }
 }
