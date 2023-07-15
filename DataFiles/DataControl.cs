1c97cd10b6d6671e6b903e1d42e6c0e1935b10d2
Bragin Anton <antonsayfermay@gmail.com>
2023-07-15 20:27:46 +0300

    Create simple realization save/load data levels

--- DataFiles/DataControl.cs
+++ DataFiles/DataControl.cs
@@ -1,22 +1,37 @@
 using Godot;
+using Godot.Collections;
 using System;
 
-public static class Data
+public static class DataControl
 {
-    
-}
+    private const string DataSavePath = "user://SavesData.json";
+    private const string LevelsDataSavePath = "user://LevelsData.json";
 
-public class DataControl
-{
-    private const string DEB_PATH = "user://DataFiles/SavesData.json";
-
-    public void SaveData()
+    public static void SaveLevels(PackedScene[] scenes)
     {
-        
+        using var file = FileAccess.Open(LevelsDataSavePath, FileAccess.ModeFlags.Write);
+
+        Dictionary dict = new();
+        foreach (var level in scenes)
+            dict.Add("Level " + (int)level.GetMeta("Id"), (bool)level.GetMeta("IsComplete"));
+
+        file.StoreLine(Json.Stringify(dict));
     }
 
-    public void LoadData() 
-    {
+    private static void SaveLevels() {
+        using var file = FileAccess.Open(LevelsDataSavePath, FileAccess.ModeFlags.Write);
+        file.StoreLine(Json.Stringify(new Dictionary()));
+    }
 
+    public static void LoadLevels(PackedScene[] scenes) 
+    {
+        using var file = FileAccess.Open(LevelsDataSavePath, FileAccess.ModeFlags.Read);
+        if (!FileAccess.FileExists(LevelsDataSavePath)) SaveLevels();
+        string text = file.GetAsText();
+
+        Dictionary dict = (Dictionary)Json.ParseString(file.GetLine());
+
+        for (int i = 0; i < scenes.Length; i++) 
+            scenes[i].SetMeta("IsComplete", dict["Level " + i]);
     }
 }
