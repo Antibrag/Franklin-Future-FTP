6dd505fff96df8e68de5dda51aa49663cc636389
Bragin Anton <antonsayfermay@gmail.com>
2023-07-19 12:33:04 +0300

    Save and load array Nodes instead of PackedScene

--- DataFiles/DataControl.cs
+++ DataFiles/DataControl.cs
@@ -7,31 +7,31 @@ public static class DataControl
     private const string DataSavePath = "user://SavesData.json";
     private const string LevelsDataSavePath = "user://LevelsData.json";
 
-    public static void SaveLevels(PackedScene[] scenes)
+    public static void SaveLevels(Node[] levels)
     {
         using var file = FileAccess.Open(LevelsDataSavePath, FileAccess.ModeFlags.Write);
 
         Dictionary dict = new();
-        foreach (var level in scenes)
+        foreach (var level in levels) 
             dict.Add("Level " + (int)level.GetMeta("Id"), (bool)level.GetMeta("IsComplete"));
 
         file.StoreLine(Json.Stringify(dict));
     }
 
-    private static void SaveLevels() {
-        using var file = FileAccess.Open(LevelsDataSavePath, FileAccess.ModeFlags.Write);
-        file.StoreLine(Json.Stringify(new Dictionary()));
-    }
-
-    public static void LoadLevels(PackedScene[] scenes) 
+    public static void LoadLevels(Node[] levels) 
     {
         using var file = FileAccess.Open(LevelsDataSavePath, FileAccess.ModeFlags.Read);
-        if (!FileAccess.FileExists(LevelsDataSavePath)) SaveLevels();
+        if (!FileAccess.FileExists(LevelsDataSavePath)) 
+            SaveLevels(levels);
+            
         string text = file.GetAsText();
 
         Dictionary dict = (Dictionary)Json.ParseString(file.GetLine());
 
-        for (int i = 0; i < scenes.Length; i++) 
-            scenes[i].SetMeta("IsComplete", dict["Level " + i]);
+        foreach (var level in levels) 
+        {
+            level.SetMeta("IsComplete", dict["Level " + (int)level.GetMeta("Id")]);
+            GD.Print("Data Saved: " + level.GetMeta("Name") + " - " + level.GetMeta("IsComplete"));
+        }
     }
 }
